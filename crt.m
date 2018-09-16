1ed25a6868398c03b0ad3f751fbb3e9965094aab
123tarunanand <anandtarun2@gmail.com>
2018-09-16 23:04:04 +0530

    Octave to Matlab syntax changes

--- crt.m
+++ crt.m
@@ -1,9 +1,9 @@
-function sol = crt(primes, residues)
-  [gcd,x_euc,y_euc] = euclid(primes(1), primes(2));
-  if gcd != 1
-    fprintf("ERROR!\nInputs are not co-prime.\n");
+function sol = crt(prime, residues)
+  [gcd,x_euc,y_euc] = euclid(prime(1), prime(2));
+  if gcd ~= 1
+    fprintf('ERROR!\nInputs are not co-prime.\n');
     sol = -1;
     return
-  endif
-  sol = primes(1)*x_euc*residues(2) + primes(2)*y_euc*residues(1);
-endfunction
+  end
+  sol = prime(1)*x_euc*residues(2) + prime(2)*y_euc*residues(1);
+end
