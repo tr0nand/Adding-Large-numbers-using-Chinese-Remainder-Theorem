eca4741efac3f93c572945c76bb0f08a1483b640
123tarunanand <anandtarun2@gmail.com>
2018-09-17 22:44:08 +0530

    Started RabinMiller

---
+++ rabinMiller.m
@@ -0,0 +1,16 @@
+%Function to implement the Rabin Miller Algorithm
+%This algorithm is used to check whether a number is prime
+%This is part of the prime number generator
+
+function res = rabinMiller(n)
+
+s = n-1
+t=0
+while s&1 ==0
+  s = s/2
+  t = t+1
+end
+k = 0
+while k < 128
+end
+end 
