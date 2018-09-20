68091b4925ddb4808a9e17704106c315b0438286
Archit Pandey <architpandeynitk@gmail.com>
2018-09-20 16:09:33 +0530

    Code linting changes

--- crt.m
+++ crt.m
@@ -1,9 +1,12 @@
 function sol = crt(prime, residues)
-  [gcd,x_euc,y_euc] = euclid(prime(1), prime(2));
-  if gcd ~= 1
-    fprintf('ERROR!\nInputs are not co-prime.\n');
-    sol = -1;
-    return
+  p_prod = prod(prime)
+  sol = 0;
+  for i=1:size(prime)(2)
+    temp_p = p_prod/prime(i);
+    [gcd,x_euc,y_euc] = euclid(prime(i),temp_p);
+    x_euc = mod(x_euc,prime(i));
+    sol = sol + residues(i)*y_euc*temp_p;
+    sol = mod(sol,p_prod);
   end
-  sol = prime(1)*x_euc*residues(2) + prime(2)*y_euc*residues(1);
+  return
 end
