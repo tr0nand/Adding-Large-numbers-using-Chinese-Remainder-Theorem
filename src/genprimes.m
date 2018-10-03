c3176bf2495afa45e058e95be334e7b5cf1ec2cd
123tarunanand <anandtarun2@gmail.com>
2018-10-03 14:55:41 +0530

    Adding prime number generator

---
+++ src/genprimes.m
@@ -0,0 +1,3 @@
+%The purpose of this function is to generate the required primes to be utilized in the crt addition
+
+function primes = genprimes(number)
--- src/main.m
+++ src/main.m
@@ -1,12 +1,14 @@
 function ans = main(num1,num2)
-  primes = [2,3,5,7,11,13,17,19,29,31,37,41,43,47,53,59,61,67,71];
+  primes = [2,3,5,7,11,13,17,19];
   residues = [];
   for i=1:length(primes)
     x = mod(num1,primes(i));
     y = mod(num2,primes(i));
+    fprintf('%d residues -%d %d\n',primes(i),x,y)
     resi = x + y;
     resi = mod(resi,primes(i));
     residues(i) = resi;
+    fprintf('%d\n',resi)
   end
   ans = crt(primes,residues);
 end
